4390d6551e5994d9634da865959a2609018b9e0c
bneo <nightcoreshi@gmail.com>
2025-04-04 20:50:41 +0330

    commit add allowhosts

--- MobileStore/settings.py
+++ MobileStore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-k@u5awgfwr!xz5eqq-mv#ky^(9!wn+by3eh!_(-l()xywj3uj9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
