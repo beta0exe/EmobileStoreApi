69538b4592e10577f94ccc38ec939887e6889ab8
bneo <nightcoreshi@gmail.com>
2025-04-04 22:19:48 +0330

    Adding New settings for middlewares

--- MobileStore/settings.py
+++ MobileStore/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "core.Middleware.JwtCookieMiddleware"
+    "core.Middleware.JwtCookieMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
